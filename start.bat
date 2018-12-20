a46dda6e23667259eb2385d2146ad82c84bb4b73
lmc999 <39726914+lmc999@users.noreply.github.com>
2018-12-20 23:57:38 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,8 +2,8 @@
 cd /d %~dp0
 %1 start mshta vbscript:createobject("wscript.shell").run("""%~0"" ::",0)(window.close)&&exit
 
-start /b D:\software\TunSafe\bat\udp2raw.exe -c -r44.55.66.77:9898 -l 127.0.0.1:9999 --raw-mode faketcp -k passwd
-start /b D:\software\TunSafe\bat\speederv2.exe -c -l0.0.0.0:2099 -r127.0.0.1:9999 -f2:4 --mode 0 --report 10 --timeout 0
+start /b udp2raw.exe -c -r44.55.66.77:9898 -l 127.0.0.1:9999 --raw-mode faketcp -k passwd
+start /b speederv2.exe -c -l0.0.0.0:2099 -r127.0.0.1:9999 -f2:4 --mode 0 --report 10 --timeout 0
 
 
 
