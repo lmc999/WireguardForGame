d55fd9e8eec5d8189e7a2e4bd25053b07cedfc05
lmc999 <39726914+lmc999@users.noreply.github.com>
2018-12-20 17:35:18 +0800

    Update wgforgame.sh

--- wgforgame.sh
+++ wgforgame.sh
@@ -77,15 +77,14 @@ wireguard_install(){
     systemctl enable iptables 
     systemctl start iptables 
     iptables -F
-	iptables -X
-	iptables -t nat -F
-	iptables -t nat -X
-	iptables -t mangle -F
-	iptables -t mangle -X
-	iptables -P INPUT ACCEPT
-	iptables -P FORWARD ACCEPT
-	iptables -P OUTPUT ACCEPT
-	
+    iptables -X
+    iptables -t nat -F
+    iptables -t nat -X
+    iptables -t mangle -F
+    iptables -t mangle -X
+    iptables -P INPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+    iptables -P OUTPUT ACCEPT
     service iptables save
     service iptables restart
     echo 1 > /proc/sys/net/ipv4/ip_forward
