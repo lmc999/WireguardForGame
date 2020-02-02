c4725054055efec609c162e55739a05bc368cf70
lmc999 <39726914+lmc999@users.noreply.github.com>
2020-02-02 13:25:55 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ cd /d %~dp0
 %1 start mshta vbscript:createobject("wscript.shell").run("""%~0"" ::",0)(window.close)&&exit
 
 start /b udp2raw.exe -c -r44.55.66.77:9898 -l 127.0.0.1:9999 --raw-mode faketcp -k passwd
-start /b speederv2.exe -c -l0.0.0.0:2099 -r127.0.0.1:9999 -f2:4 --mode 0 --report 10 --timeout 0
+start /b speederv2.exe -c -l0.0.0.0:2099 -r127.0.0.1:9999 -f2:4 --mode 0 --report 10 --timeout 2
 
 
 
